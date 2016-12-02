f5d021ad3c5d4db15a6646e284c39a98b74415ff
xiafeng <xiafeng@xiaokakeji.com>
2016-12-02 10:42:02 +0800

    上传代码

---
+++ Public/js/Home/footer.js
@@ -0,0 +1,12 @@
+//baidu tongji
+var _hmt = _hmt || [];
+(function() {
+})();
+function footer()
+{
+    var UA = navigator.userAgent.toLowerCase();
+    if((UA.indexOf('iphone') != -1 || UA.indexOf('mobile') != -1 || UA.indexOf('android') != -1 || UA.indexOf('ipad') != -1 || UA.indexOf('windows ce') != -1 || UA.indexOf('ipod') != -1) && UA.indexOf('ipod') == -1) {
+        location.href = 'http://dl.xiaokakeji.com/';
+        return;
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # left-menu
 left-menu html code
+
+参考https://open.weixin.qq.com/cgi-bin/showdocument?action=dir_list&t=resource/res_list&verify=1&id=1417674108&token=&lang=zh_CN
+微信开放平台资源中心页面做的左侧菜单和对应内容的展示
